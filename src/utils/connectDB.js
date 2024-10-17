596b782ecbe4ba31726dc9ad390314e1a5e6ed01
hallstrom91 <simonhallstrom@live.se>
2024-10-17 10:46:26 +0200

    #6 - add database connection utils

---
+++ src/utils/connectDB.js
@@ -0,0 +1,12 @@
+require("dotenv").config({ path: [".env"] });
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+});
+
+module.exports = pool;
