a449c1a625d16cadd83206495eca444961fe6c8c
hallstrom91 <simonhallstrom@live.se>
2024-10-17 10:43:12 +0200

    #3 - add entry point for application

---
+++ src/index.js
@@ -0,0 +1,7 @@
+require("dotenv").config({ path: [".env.development.local", ".env"] });
+const app = require("./service.js");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Domain server is live on port: ${PORT}`);
+});
