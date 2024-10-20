02493e37ab5040b08c1f22b34d4053c7b60da372
hallstrom91 <simonhallstrom@live.se>
2024-10-20 18:01:20 +0200

    #21, #22, #23 added new routes to application

--- src/service.js
+++ src/service.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const test = require("./routes/userRoutes.js");
+const cookieParser = require("cookie-parser");
+const cookieRoutes = require("./routes/cookieRoutes.js");
+const boardRoutes = require("./routes/boardRoutes.js");
+const taskRoutes = require("./routes/taskRoutes.js");
 const app = express();
 
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
-test();
+app.use(cookieParser());
 
+app.use("/cookie", cookieRoutes);
+app.use("/board", boardRoutes);
+app.use("/task", taskRoutes);
 module.exports = app;
