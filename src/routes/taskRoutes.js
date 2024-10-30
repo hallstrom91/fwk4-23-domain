1ec4ac0f6f2df779299366f32182888e298eb22b
Simon Hällström <simonhallstrom@live.se>
2024-10-30 12:31:34 +0100

    #21 - Added more routes for tasks

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,20 +1,19 @@
 const express = require("express");
-const {
-  createTask,
-  getTaskForBoard,
-  openTask,
-  addTaskComment,
-} = require("../controllers/taskControllers.js");
 const { verifyJwt } = require("../utils/jwtUtils.js");
-
+const taskControllers = require("../controllers/task");
 const router = express.Router();
 
-router.post("/create/:boardId", verifyJwt, createTask);
-
-router.get("/:boardId/tasks", verifyJwt, getTaskForBoard);
-
-router.get("/:boardId/open/:taskId", verifyJwt, openTask);
-
-router.post("/addcomment/:taskId", verifyJwt, addTaskComment);
+router.post("/create/:boardId", verifyJwt, taskControllers.createTask);
+router.get("/:boardId/tasks", verifyJwt, taskControllers.getTaskForBoard);
+router.get("/:boardId/open/:taskId", verifyJwt, taskControllers.openTask);
+router.post("/addcomment/:taskId", verifyJwt, taskControllers.addTaskComment);
+router.put("/update/:taskId", verifyJwt, taskControllers.updateTask);
+router.put("/update/:commentId", verifyJwt, taskControllers.updateTaskComment);
+router.delete("/delete/:taskId", verifyJwt, taskControllers.deleteTask);
+router.delete(
+  "/delete/:commentId",
+  verifyJwt,
+  taskControllers.deleteTaskComment,
+);
 
 module.exports = router;
