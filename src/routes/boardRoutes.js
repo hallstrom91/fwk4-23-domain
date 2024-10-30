c14b41134923c924b8d4060e51f5269da1239912
Simon Hällström <simonhallstrom@live.se>
2024-10-30 12:32:05 +0100

    #20 - Added more routes for boards

--- src/routes/boardRoutes.js
+++ src/routes/boardRoutes.js
@@ -1,17 +1,26 @@
 const express = require("express");
-const {
-  createBoard,
-  addBoardMember,
-  getUserBoards,
-} = require("../controllers/boardControllers.js");
 const { verifyJwt } = require("../utils/jwtUtils.js");
+const boardController = require("../controllers/board");
 
 const router = express.Router();
 
-router.post("/create", verifyJwt, createBoard);
+router.post("/create", verifyJwt, boardController.createBoard);
+router.get("/my-boards", verifyJwt, boardController.getAllUserBoards);
+router.post("/adduser/:boardId", verifyJwt, boardController.addBoardMember);
 
-router.get("/my-boards", verifyJwt, getUserBoards);
-
-router.post("/adduser/:boardId", verifyJwt, addBoardMember);
+// new routes
+router.get("/:boardId", verifyJwt, boardController.getBoardWithTasks);
+router.put("/update/:boardId", verifyJwt, boardController.updateBoard);
+router.put(
+  "/:boardId/updateuser/:userIdToUpdate",
+  verifyJwt,
+  boardController.updateBoardMemberRole,
+);
+router.delete(
+  "/:boardId/removeuser/:userIdToRemove",
+  verifyJwt,
+  boardController.removeBoardMember,
+);
+router.delete("/deleteboard/:boardId", verifyJwt, boardController.deleteBoard);
 
 module.exports = router;
