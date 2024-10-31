c14cf654ec1ccf6ea0513e4d26d4dfd40806c944
Simon Hällström <simonhallstrom@live.se>
2024-10-31 16:19:46 +0100

    #39 - Add express-rate-limit

--- src/service.js
+++ src/service.js
@@ -1,17 +1,27 @@
 const express = require("express");
+const app = express();
 const helmet = require("helmet");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const rateLimit = require("express-rate-limit");
+
 const cookieRoutes = require("./routes/cookieRoutes.js");
 const boardRoutes = require("./routes/boardRoutes.js");
 const taskRoutes = require("./routes/taskRoutes.js");
 const inviteRoutes = require("./routes/inviteRoutes.js");
-const app = express();
 
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(cookieParser());
+
+const requestLimit = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "To many requests, madman!",
+});
+
+app.use(requestLimit);
 
 app.use("/cookie", cookieRoutes);
 app.use("/board", boardRoutes);
